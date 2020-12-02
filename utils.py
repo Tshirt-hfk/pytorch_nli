69737cba9a9851354e7a6668aaa6b2e01350fa94
Tshirt-hfk <fanglei212@outlook.com>
2020-12-02 21:41:42 +0800

    zc

--- utils.py
+++ utils.py
@@ -25,7 +25,7 @@ def parse_args():
     parser.add_argument('--attention-dropout', type=float, default=0.)
 
     parser.add_argument('--epochs', type=int, default=100)
-    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--lr', type=float, default=3e-4)
     parser.add_argument('--weight-decay', type=float, default=1/600)
 
     parser.add_argument('--results_dir', type=str, default='results')
