972a419d6a161ff9823a12128a15fd731798fcde
Tshirt-hfk <fanglei212@outlook.com>
2020-12-03 14:00:47 +0800

    zc

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ class Train():
 
         self.model.to(self.device)
         self.criterion = nn.CrossEntropyLoss(reduction='sum')
-        self.opt = optim.AdamW(self.model.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
+        self.opt = optim.Adam(self.model.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
         self.best_val_acc = None
 
         print("resource preparation done: {}".format(datetime.datetime.now()))
